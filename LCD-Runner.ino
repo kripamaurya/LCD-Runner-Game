2f310f5fcc3a50984aec6da16f6a9731b6ebe6d4
Kripa Shankar Maurya <mkripa456@gmail.com>
2026-05-12 08:58:33 +0530

    Add files via upload

---
+++ LCD-Runner.ino
@@ -0,0 +1,111 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+const int buttonPin = 7;
+
+int playerRow = 1;
+int obstaclePos = 15;
+int score = 0;
+
+bool gameOver = false;
+
+// Player Character
+byte player[8] = {
+  B00100,
+  B01110,
+  B00100,
+  B01110,
+  B10101,
+  B00100,
+  B01010,
+  B10001
+};
+
+void setup() {
+
+  lcd.init();
+  lcd.backlight();
+
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  lcd.createChar(0, player);
+
+  lcd.clear();
+}
+
+void resetGame() {
+
+  obstaclePos = 15;
+  score = 0;
+  gameOver = false;
+
+  lcd.clear();
+}
+
+void loop() {
+
+  // Restart Game
+  if (gameOver) {
+
+    if (digitalRead(buttonPin) == LOW) {
+
+      delay(200);
+      resetGame();
+    }
+
+    return;
+  }
+
+  // Jump
+  if (digitalRead(buttonPin) == LOW) {
+    playerRow = 0;
+  } else {
+    playerRow = 1;
+  }
+
+  lcd.clear();
+
+  // Score on Right Side
+  lcd.setCursor(6, 0);
+  lcd.print("Score:");
+  lcd.print(score);
+
+  // Player
+  lcd.setCursor(1, playerRow);
+  lcd.write(byte(0));
+
+  // Obstacle
+  lcd.setCursor(obstaclePos, 1);
+  lcd.print("|");
+
+  // Collision
+  if (obstaclePos == 1 && playerRow == 1) {
+
+    lcd.clear();
+
+    lcd.setCursor(2, 0);
+    lcd.print("GAME OVER");
+
+    lcd.setCursor(2, 1);
+    lcd.print("Score:");
+    lcd.print(score);
+
+    gameOver = true;
+
+    return;
+  }
+
+  // Move Obstacle
+  obstaclePos--;
+
+  // Increase Score
+  if (obstaclePos < 0) {
+
+    obstaclePos = 15;
+    score++;
+  }
+
+  delay(150);
+}
